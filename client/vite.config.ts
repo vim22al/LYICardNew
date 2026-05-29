a60c0ff0d53590f8d18d0a9506984c7a4aaabf96
Vimal Kumar <vim22al@gmail.com>
2026-05-29 20:17:58 +0530

    added netlify code

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,10 +5,11 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  plugins: [devtools(), netlify(), tailwindcss(), tanstackStart(), viteReact()],
 })
 
 export default config
